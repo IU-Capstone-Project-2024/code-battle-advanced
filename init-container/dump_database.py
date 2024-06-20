bd63d244e01fb17fe0cc483ced6b04f4abc35542
tjann7 <t.suleymanov@innopolis.university>
2024-06-20 14:03:40 +0300

    test

---
+++ init-container/dump_database.py
@@ -0,0 +1,28 @@
+import time
+import pymongo
+import bson
+import subprocess
+from sys import stderr
+import redis
+import uuid
+import hashlib
+import bcrypt
+import os
+import base64
+
+mongo_uri_docker = "mongodb://sUbskr1bet0:1celypuZZl3s@192.168.49.2:32000/CBA_database?authSource=admin"
+
+client = pymongo.MongoClient(mongo_uri_docker)
+db = client['CBA_database']
+
+while 1:
+    try:
+        client.server_info()
+        break
+    except pymongo.errors.ServerSelectionTimeoutError:
+        continue
+
+for i in db.list_collection_names():
+    print(f"{i}:")
+    print(bson.json_util.dumps(list(db[i].find()), indent=4))
+
