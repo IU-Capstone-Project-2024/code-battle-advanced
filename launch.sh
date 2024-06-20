cc652a62cbd1683c9330dcc20cd554a4d2d28456
Timur Suleymanov <t.suleymanov@innopolis.university>
2024-06-20 11:35:40 +0000

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -9,8 +9,10 @@ minikube addons enable metrics-server &
 
 docker build -t web_app:v8 ./webapp-container
 docker build -t judge:latest ./judge-container
+docker build -t init_database:latest ./init-container
 minikube image load web_app:v8
 minikube image load judge
+minikube image load init_database
 
 minikube kubectl -- apply -f ./kubernetes-configs
 
