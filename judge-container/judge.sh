1f8e8c9a8010dab63440182b34f8c1357c3de75b
Magel0n <i.lishchenko@innopolis.university>
2024-06-30 21:29:26 +0300

    Added java in Judge.sh and changed 30 tests, also changed some app.py from Leha

--- judge-container/judge.sh
+++ judge-container/judge.sh
@@ -5,6 +5,12 @@ task=$2
 compiler=$3
 mstime=$4
 numberOfFiles=$(ls -1 ./tasks/$task/tests| wc -l)
+code=$1
+task="A+B"
+compiler=$3
+mstime=$4
+numberOfFiles=$(ls -1 ./tasks/$task/tests| wc -l)
+
 if [ ! -e "./tasks/$2/checker.sh" ]
 then
 	>&2 echo There is no checker.sh to check the solutions
@@ -15,42 +21,60 @@ if [ "$compiler" = "cpp" ]; then
 	g++ -x c++ $code -o compiledCode
 	exitCode=$?
 	if [ ! $exitCode -eq 0 ]  ; then
-		echo CE 0 0
+		echo CE
 		exit
 	fi
+fi
+
+if [ "$compiler" = "java" ]; then
+	javac -d "$PWD" $code
+	exitCode=$?
+	if [ ! $exitCode -eq 0 ]  ; then
+		echo CE
+		exit
+	fi
+	compiledClass=${code##*/}
+	compiledClass=${compiledClass%?????}
 fi
 
 for fileId in $(seq 0 $(($numberOfFiles - 1))) 
 do
 	file="./tasks/${task}/tests/${fileId}.in"
 	if [ -f "$file" ]; then
-	
 		if [ "$compiler" = "python3" ]; then 
 			cat $file | timeout -s 9 $(echo $mstime / 1000 | bc -l)s python3 -I $code > temp.out
 			exitCode=$?
 		else if [ "$compiler" = "cpp" ]; then 
 			cat $file | timeout -s 9 $(echo $mstime / 1000 | bc -l)s ./compiledCode > temp.out
 			exitCode=$?
+		else if [ "$compiler" = "java" ]; then 
+			cat $file | timeout -s 9 $(echo $mstime / 1000 | bc -l)s java $compiledClass > temp.out
+			exitCode=$?
 		else
 			>&2 echo Unsupported compiler option
 			exit
-		fi fi # Cool way to see the number of compilers
+		fi fi fi # Cool way to see the number of compilers
+		
 		
 		if [ $exitCode -eq 137 ]  ; then
-			echo TL $fileId 0 '\n'
+			echo TL $fileId
 		else if [ $exitCode -eq 124 ]  ; then
-			echo TL $fileId 0 '\n'
+			echo TL $fileId
 		else if [ ! $exitCode -eq 0 ]  ; then
-			echo RE $fileId 0 '\n'
+			echo RE $fileId
 		else if [ ! $(sh ./tasks/$task/checker.sh $file temp.out) = "True" ] ; then
-			echo WA $fileId 0 '\n'
+			echo WA $fileId
 		else
-			echo AC $fileId 1 '\n'
+			echo AC $fileId
 		fi fi fi fi
 		rm temp.out
 	fi
 done
 	
+	
+if [ "$compiler" = "java" ]; then
+	rm $compiledClass.class
+fi
 # echo AC
 exit
 
