e5b8ed3936afb5457629a43d1f387a0d31ebae09
Magel0n <i.lishchenko@innopolis.university>
2024-07-11 19:46:19 +0300

    Changed from Alpine to Debian

--- judge-container/judge.sh
+++ judge-container/judge.sh
@@ -14,23 +14,27 @@ then
 fi
 
 if [ "$compiler" = "cpp" ]; then 
-	g++ -x c++ $code -o compiledCode
+	g++ -x c++ $code -o compiledCode 2> temperr.out
 	exitCode=$?
 	if [ ! $exitCode -eq 0 ]  ; then
-		echo CE 0 0
+		echo CE 0 0 -1
+		rm temperr.out
 		exit
 	fi
+	rm temperr.out
 fi
 
 if [ "$compiler" = "java" ]; then
-	javac -d "$PWD" $code
+	javac -d "$PWD" $code 2> temperr.out
 	exitCode=$?
 	if [ ! $exitCode -eq 0 ]  ; then
-		echo CE 0 0
+		echo CE 0 0 -1
+		rm temperr.out
 		exit
 	fi
 	compiledClass=${code##*/}
 	compiledClass=${compiledClass%?????}
+	rm temperr.out
 fi
 
 for fileId in $(seq 0 $(($numberOfFiles - 1))) 
@@ -38,35 +42,35 @@ do
 	file="./tasks/${task}/tests/${fileId}.in"
 	if [ -f "$file" ]; then
 		if [ "$compiler" = "python3" ]; then 
-			cat $file | timeout -s 9 $(echo \($mstime+10\)/1000 | bc -l)s time -f "%U" -o temp2.out python3 -I $code > temp.out
+			cat $file | timeout -s 9 $(echo \($mstime+10\)/1000 | bc -l)s time -f "%U" -o temp2.out python3 -I $code 1> temp.out 2> temperr.out
 			exitCode=$?
 		else if [ "$compiler" = "cpp" ]; then 
-			cat $file | timeout -s 9 $(echo \($mstime+10\)/1000 | bc -l)s time -f "%U" -o temp2.out ./compiledCode > temp.out
+			cat $file | timeout -s 9 $(echo \($mstime+10\)/1000 | bc -l)s time -f "%U" -o temp2.out ./compiledCode 1> temp.out 2> temperr.out
 			exitCode=$?
 		else if [ "$compiler" = "java" ]; then 
-			cat $file | timeout -s 9 $(echo \($mstime+10\)/1000 | bc -l)s time -f "%U" -o temp2.out java $compiledClass > temp.out 
+			cat $file | timeout -s 9 $(echo \($mstime+10\)/1000 | bc -l)s time -f "%U" -o temp2.out java $compiledClass 1> temp.out 2> temperr.out 
 		else
 			>&2 echo Unsupported compiler option
 			exit
 		fi fi fi # Cool way to see the number of compilers
 		
-		
 		if [ $exitCode -eq 137 ]  ; then
-			echo TL $fileId 0 -1.0
+			echo TL $fileId 0 -1
 		else if [ $exitCode -eq 124 ]  ; then
-			echo TL $fileId 0 -1.0
-		else if [ $( echo $(cat temp2.out)*1000 | bc -l | cut -d. -f1 ) -gt $mstime ] ; then
-			echo TL $fileId 0 -1.0
+			echo TL $fileId 0 -1
 		else if [ ! $exitCode -eq 0 ]  ; then
-			echo RE $fileId 0 -1.0
+			echo RE $fileId 0 -1
+		else if [ $( echo $(cat temp2.out)*1000 | bc -l | cut -d. -f1 ) -gt $mstime ] ; then
+			echo TL $fileId 0 -1
 		else if [ ! $(sh ./tasks/$task/checker.sh $file temp.out) = "True" ] ; then
-			echo WA $fileId 0 -1.0
+			echo WA $fileId 0 -1
 		else
 			echo AC $fileId 1 $(echo $(cat temp2.out)*1000 | bc -l | cut -d. -f1)
 		fi fi fi fi fi
 		
 		rm temp.out
 		rm temp2.out
+		rm temperr.out
 	fi
 done
 	
--- launch.sh
+++ launch.sh
@@ -11,10 +11,10 @@ python3 -m grpc_tools.protoc   --proto_path=./contest-container   --python_out=.
 cp ./contest-container/contest_pb2.py ./contest-container/contest_pb2.pyi ./contest-container/contest_pb2_grpc.py -t ./judge-container
 cp ./contest-container/contest_pb2.py ./contest-container/contest_pb2.pyi ./contest-container/contest_pb2_grpc.py -t ./webapp-container
 
-docker build -t web_app:v8 ./webapp-container
-docker build -t judge:latest ./judge-container
-docker build -t init_database:latest ./init-container
-docker build -t contest:latest ./contest-container
+docker build --network host -t web_app:v8 ./webapp-container
+docker build --network host -t judge:latest ./judge-container
+docker build --network host -t init_database:latest ./init-container
+docker build --network host -t contest:latest ./contest-container
 minikube image load web_app:v8
 minikube image load judge
 minikube image load init_database
